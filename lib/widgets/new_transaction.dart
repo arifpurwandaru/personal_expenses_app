20bddd6eda744f834c88ee0512b278ddbd1392ea
arifpurwandaru <ArifNazar.Purwandaru@mitrais.com>
2019-07-31 08:31:30 +0700

    106. Getting Started With Datepick

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -42,10 +42,27 @@ class _NewTransactionState extends State<NewTransaction> {
               keyboardType: TextInputType.number,
               onSubmitted: (_) => submitTrx(),
             ),
-            FlatButton(
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Text('No Date Chosen!'),
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+            RaisedButton(
               child: Text('Add Transaction'),
               onPressed: submitTrx,
-              textColor: Colors.purple,
+              color: Theme.of(context).primaryColor,
+              textColor: Theme.of(context).textTheme.button.color,
             )
           ],
         ),
